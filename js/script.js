691148217165ddcc4b7360366b584f715fd6c4fe
suchil19 <jlsuchil@gmail.com>
2024-01-25 20:34:39 -0600

    primer commit

--- js/script.js
+++ js/script.js
@@ -1,3 +1,8 @@
+// Preloader
+$(window).on('load', function() {
+  $('#preloader').delay(100).fadeOut('slow',function(){$(this).remove();});
+});
+
 $(document).ready(function(){
     $('.btn').click(function(){
       $('.items').toggleClass("show");
